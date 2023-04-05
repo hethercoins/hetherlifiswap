3fdb2a64c4ed1abdf53038ef25cdaf4d0da25cf2
esculapeso <103459392+esculapeso@users.noreply.github.com>
2023-04-05 13:07:09 +0200

    changed default tokens

--- examples/nextjs/components/Widget.tsx
+++ examples/nextjs/components/Widget.tsx
@@ -7,6 +7,11 @@ export const Widget = () => {
       <WidgetEvents />
       <LiFiWidget
         config={{
+          fromChain: 137,
+          toChain: 137,
+          fromToken: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+          toToken: '0x2b0f99d8e85227784fb0b7bf0b6ede6458741eee',
+          fromAmount: 10,
           containerStyle: {
             border: `1px solid rgb(234, 234, 234)`,
             borderRadius: '16px',
